774284be5768cb5119eb4b3c10a7251e594a3218
Stephen M. Cameron <stephenmcameron@gmail.com>
2019-12-13 14:39:53 -0500

    smashout: Add paddle to smashout game
    Signed-off-by: Stephen M. Cameron <stephenmcameron@gmail.com>

--- badge_apps/smashout.c
+++ badge_apps/smashout.c
@@ -34,6 +34,11 @@ extern char *strcat(char *dest, const char *src);
 /* TODO figure out where these should really come from */
 #define SCREEN_XDIM 132
 #define SCREEN_YDIM 132
+#define PADDLE_HEIGHT 15
+
+static struct smashout_paddle {
+	int x, y, w;
+} paddle, old_paddle;
 
 /* Program states.  Initial state is SMASHOUT_GAME_INIT */
 enum smashout_program_state_t {
@@ -46,10 +51,39 @@ static enum smashout_program_state_t smashout_program_state = SMASHOUT_GAME_INIT
 
 static void smashout_game_init(void)
 {
+	FbInit();
+	FbClear();
+	paddle.x = SCREEN_XDIM / 2;
+	paddle.y = SCREEN_YDIM - PADDLE_HEIGHT;
+	paddle.w = 10;
+	old_paddle = paddle;
+	smashout_program_state = SMASHOUT_GAME_PLAY;
+}
+
+static void smashout_check_buttons()
+{
+}
+
+static void smashout_draw_paddle()
+{
+	int dx = old_paddle.w / 2;
+	FbColor(BLACK);
+	FbHorizontalLine(old_paddle.x - dx, old_paddle.y, old_paddle.x + dx, old_paddle.y);
+	FbColor(WHITE);
+	FbHorizontalLine(paddle.x - dx, paddle.y, paddle.x + dx, paddle.y);
+	old_paddle = paddle;
+}
+
+static void smashout_draw_screen()
+{
+	smashout_draw_paddle();
+	FbSwapBuffers();
 }
 
 static void smashout_game_play()
 {
+	smashout_check_buttons();
+	smashout_draw_screen();
 }
 
 static void smashout_game_exit()
