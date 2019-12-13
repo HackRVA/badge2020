dd8836416d479c8abf4f0e43cf1e1cb012bb7469
Stephen M. Cameron <stephenmcameron@gmail.com>
2019-12-13 14:39:53 -0500

    smashout: add ball to smashout game
    Signed-off-by: Stephen M. Cameron <stephenmcameron@gmail.com>

--- badge_apps/smashout.c
+++ badge_apps/smashout.c
@@ -35,10 +35,20 @@ extern char *strcat(char *dest, const char *src);
 #define SCREEN_XDIM 132
 #define SCREEN_YDIM 132
 #define PADDLE_HEIGHT 15
+#define PADDLE_WIDTH 20
+#define PADDLE_SPEED 10
+#define BALL_STARTX (SCREEN_XDIM / 2)
+#define BALL_STARTY (SCREEN_YDIM / 2)
+#define BALL_START_VX 3
+#define BALL_START_VY 2
 
 static struct smashout_paddle {
 	int x, y, w;
 } paddle, old_paddle;
+
+static struct smashout_ball {
+	int x, y, vx, vy;
+} ball, oldball;
 
 /* Program states.  Initial state is SMASHOUT_GAME_INIT */
 enum smashout_program_state_t {
@@ -55,20 +65,24 @@ static void smashout_game_init(void)
 	FbClear();
 	paddle.x = SCREEN_XDIM / 2;
 	paddle.y = SCREEN_YDIM - PADDLE_HEIGHT;
-	paddle.w = 10;
+	paddle.w = PADDLE_WIDTH;
 	old_paddle = paddle;
+	ball.x = BALL_STARTX;
+	ball.y = BALL_STARTY;
+	ball.vx = BALL_START_VX;
+	ball.vy = BALL_START_VY;
+	oldball = ball;
 	smashout_program_state = SMASHOUT_GAME_PLAY;
 }
 
 static void smashout_check_buttons()
 {
-	const int paddle_speed = 5;
 	if (LEFT_BTN_AND_CONSUME) {
-		paddle.x = paddle.x - paddle_speed;
+		paddle.x = paddle.x - PADDLE_SPEED;
 		if (paddle.x < paddle.w / 2)
 			paddle.x = paddle.w / 2;
 	} else if (RIGHT_BTN_AND_CONSUME) {
-		paddle.x = paddle.x + paddle_speed;
+		paddle.x = paddle.x + PADDLE_SPEED;
 		if (paddle.x > SCREEN_XDIM - paddle.w / 2)
 			paddle.x = SCREEN_XDIM - paddle.w / 2;
 	}
@@ -84,15 +98,58 @@ static void smashout_draw_paddle()
 	old_paddle = paddle;
 }
 
+static void smashout_draw_ball()
+{
+	FbColor(BLACK);
+	FbHorizontalLine(oldball.x - 1, oldball.y - 1, oldball.x + 1, oldball.y - 1);
+	FbHorizontalLine(oldball.x - 1, oldball.y + 1, oldball.x + 1, oldball.y + 1);
+	FbVerticalLine(oldball.x - 1, oldball.y - 1, oldball.x - 1, oldball.y + 1);
+	FbVerticalLine(oldball.x + 1, oldball.y - 1, oldball.x + 1, oldball.y + 1);
+	FbColor(WHITE);
+	FbHorizontalLine(ball.x - 1, ball.y - 1, ball.x + 1, ball.y - 1);
+	FbHorizontalLine(ball.x - 1, ball.y + 1, ball.x + 1, ball.y + 1);
+	FbVerticalLine(ball.x - 1, ball.y - 1, ball.x - 1, ball.y + 1);
+	FbVerticalLine(ball.x + 1, ball.y - 1, ball.x + 1, ball.y + 1);
+}
+
+static void smashout_move_ball()
+{
+	oldball = ball;
+	ball.x = ball.x + ball.vx;
+	ball.y = ball.y + ball.vy;
+	if (ball.x > SCREEN_XDIM - 2) {
+		ball.x = SCREEN_XDIM - 2;
+		if (ball.vx > 0)
+			ball.vx = -ball.vx;
+	}
+	if (ball.y > SCREEN_YDIM - 2) {
+		ball.y = SCREEN_YDIM - 2;
+		if (ball.vy > 0)
+			ball.vy = -ball.vy;
+	}
+	if (ball.x < 1) {
+		ball.x = 1;
+		if (ball.vx < 0)
+			ball.vx = -ball.vx;
+	}
+	if (ball.y < 1) {
+		ball.y = 1;
+		if (ball.vy < 0)
+			ball.vy = -ball.vy;
+	}
+}
+
 static void smashout_draw_screen()
 {
 	smashout_draw_paddle();
+	smashout_draw_ball();
 	FbSwapBuffers();
 }
 
 static void smashout_game_play()
 {
 	smashout_check_buttons();
+	smashout_move_ball();
 	smashout_draw_screen();
 }
 
@@ -121,7 +178,7 @@ int smashout_cb(void)
 #ifdef __linux__
 int main(int argc, char *argv[])
 {
-        start_gtk(&argc, &argv, smashout_cb, 240);
+        start_gtk(&argc, &argv, smashout_cb, 30);
         return 0;
 }
 #endif
