475aeea8e6f104cd5e641a53fbba41326d3dfb73
pacna <31336845+pacna@users.noreply.github.com>
2021-10-10 14:41:19 -0400

    Rename myprogram to game_of_life

--- badge_apps/game_of_life.c
+++ badge_apps/game_of_life.c
@@ -2,102 +2,6 @@
 
 This is a badge app template.
 
-To use this file, do the following steps:
-
-1. Copy the file to a different name, the name of your app.
-   e.g. cp badge-app-template.c myapp.c
-
-   (Don't call it "myapp" please.)
-
-2. Copy include/badge-app-template.h to include/myapp.h
-
-   (Don't call it "myapp" please.)
-
-2. Edit the file.  Change myprogram_cb to some other name, e.g. "myapp_cb"
-   wherever it appears in this file. (Think of a real name, don't actually
-   use myapp_cb, please.)
-
-3. Modify linux/Makefile to build your program, for example:
-
-	diff --git a/linux/Makefile b/linux/Makefile
-	index 83034ce..b1b75f8 100644
-	--- a/linux/Makefile
-	+++ b/linux/Makefile
-	@@ -13,6 +13,7 @@ LASERTAG=lasertag
- 	 SMASHOUT=smashout
- 	 MAZE=maze
- 	 USERNAME=username
-	+MYAPP=myapp
- 
- 	-all:   ${LINUX_OBJS} bin/${MAZE} bin/${IRXMIT} bin/${LASERTAG} bin/badge_monsters bin/fixup_monster bin/username bin/sample_app
-	+all:   ${LINUX_OBJS} bin/${MAZE} bin/${IRXMIT} bin/${LASERTAG} bin/badge_monsters bin/fixup_monster bin/username bin/sample_app bin/myapp
-
- 	
-	@@ -63,6 +64,9 @@ bin/${LASERTAG}:      ${BIN} ${APPDIR}/${LASERTAG}.c linuxcompat.o bline.o
- 	 bin/${SMASHOUT}:       ${BIN} ${APPDIR}/${SMASHOUT}.c linuxcompat.o
-        	$(CC) ${CFLAGS} ${GTKCFLAGS} ${LINUX_OBJS} -o bin/${SMASHOUT} -I . -I ${BADGE_INC} ${APPDIR}/${SMASHOUT}.c ${GTKLDFLAGS}
- 	
-	+bin/${MYAPP}:  ${BIN} ${APPDIR}/${MYAPP}.c linuxcompat.o
-	+       $(CC) ${CFLAGS} ${GTKCFLAGS} ${LINUX_OBJS} -o bin/${MYAPP} -I . -I ${BADGE_INC} ${APPDIR}/${MYAPP}.c ${GTKLDFLAGS}
-	+
- 	 bin/${MAZE}:   ${BIN} ${APPDIR}/${MAZE}.c linuxcompat.o bline.o xorshift.o achievements.o\
-                        ${APPDIR}/bones_points.h \
-                        ${BADGE_INC}/build_bug_on.h \
-
-4. Modify the main Makefile, for example:
-
-	diff --git a/Makefile b/Makefile
-	index 999e7ec..c16fc6c 100644
-	--- a/Makefile
-	+++ b/Makefile
-	@@ -51,7 +51,7 @@ SRC_APPS_C = \
-        	badge_apps/blinkenlights.c badge_apps/conductor.c \
-        	badge_apps/lasertag.c badge_apps/QC.c ${IRXMIT} \
-        	badge_apps/username.c badge_apps/badge_monsters.c \
-	-       badge_apps/smashout.c
-	+       badge_apps/smashout.c badge_apps/badge-app-template.c
- 	
- 	 SRC_USB_C = USB/usb_device.c  USB/usb_function_cdc.c USB/usb_descriptors.c
-
-5. Modify src/menu.c to add your app, for example:
-
-	diff --git a/src/menu.c b/src/menu.c
-	index efefd24..b56cb44 100644
-	--- a/src/menu.c
-	+++ b/src/menu.c
-	@@ -16,6 +16,7 @@
- 	 #include "blinkenlights.h"
- 	 #include "adc.h"
- 	 #include "smashout.h"
-	+#include "badge-app-template.h"
- 	 #include "QC.h"
- 	 #include "lasertag.h"
- 	 #include "username.h"
-	@@ -499,6 +500,7 @@ const struct menu_t games_m[] = {
-    	 {"Conductor",     VERT_ITEM, FUNCTION, {(struct menu_t *)conductor_cb}}, // Tell other badges to play notes
-    	 {"Sensors",       VERT_ITEM, FUNCTION, {(struct menu_t *)adc_cb} },
-    	 {"Smash Out",     VERT_ITEM, FUNCTION, {(struct menu_t *)smashout_cb} },
-	+   {"My App",      VERT_ITEM, FUNCTION, {(struct menu_t *)myapp_cb} },
-    	 {"Laser Tag",     VERT_ITEM, FUNCTION, {(struct menu_t *)lasertag_cb} },
-    	 {"Badge Monsters",VERT_ITEM, FUNCTION, {(struct menu_t *)badge_monsters_cb} },
- 	 #ifdef INCLUDE_IRXMIT
-
-6.  Build the linux program:
-
-    cd linux
-    make bin/myapp
-
-    Or whatever you called it, (you didn't call it myapp, right?)
-
-7.  Build the main program:
-
-    cd ..
-    make clean
-    make
-
-8.  Delete all these instruction comments from your copy of the file.
-
-9.  Modify the program to make it do what you want.
 
 **********************************************/
 #ifdef __linux__
@@ -123,21 +27,21 @@ extern char *strcat(char *dest, const char *src);
 
 #include "build_bug_on.h"
 
-/* Program states.  Initial state is MYPROGRAM_INIT */
-enum myprogram_state_t
+/* Program states.  Initial state is GAME_OF_LIFE_INIT */
+enum game_of_life_state_t
 {
-	MYPROGRAM_INIT,
-	MYPROGRAM_RUN,
-	MYPROGRAM_EXIT,
+	GAME_OF_LIFE_INIT,
+	GAME_OF_LIFE_RUN,
+	GAME_OF_LIFE_EXIT,
 };
 
-static enum myprogram_state_t myprogram_state = MYPROGRAM_INIT;
+static enum game_of_life_state_t game_of_life_state = GAME_OF_LIFE_INIT;
 
-static void myprogram_init(void)
+static void game_of_life_init(void)
 {
 	FbInit();
 	FbClear();
-	myprogram_state = MYPROGRAM_RUN;
+	game_of_life_state = GAME_OF_LIFE_RUN;
 }
 
 static void check_buttons()
@@ -145,7 +49,7 @@ static void check_buttons()
 	if (BUTTON_PRESSED_AND_CONSUME)
 	{
 		/* Pressing the button exits the program. You probably want to change this. */
-		myprogram_state = MYPROGRAM_EXIT;
+		game_of_life_state = GAME_OF_LIFE_EXIT;
 	}
 	else if (LEFT_BTN_AND_CONSUME)
 	{
@@ -164,36 +68,35 @@ static void check_buttons()
 static void draw_screen()
 {
 	FbColor(WHITE);
-	FbMove(10, 50 / 2);
+	FbMove(10, 40 / 2);
 	FbWriteLine("Game of Life");
 	FbSwapBuffers();
 }
 
-static void myprogram_run()
+static void game_of_life_run()
 {
 	check_buttons();
 	draw_screen();
 }
 
-static void myprogram_exit()
+static void game_of_life_exit()
 {
-	myprogram_state = MYPROGRAM_INIT; /* So that when we start again, we do not immediately exit */
+	game_of_life_state = GAME_OF_LIFE_INIT; /* So that when we start again, we do not immediately exit */
 	returnToMenus();
 }
 
-/* You will need to rename myprogram_cb() something else. */
 int game_of_life_cb(void)
 {
-	switch (myprogram_state)
+	switch (game_of_life_state)
 	{
-	case MYPROGRAM_INIT:
-		myprogram_init();
+	case GAME_OF_LIFE_INIT:
+		game_of_life_init();
 		break;
-	case MYPROGRAM_RUN:
-		myprogram_run();
+	case GAME_OF_LIFE_RUN:
+		game_of_life_run();
 		break;
-	case MYPROGRAM_EXIT:
-		myprogram_exit();
+	case GAME_OF_LIFE_EXIT:
+		game_of_life_exit();
 		break;
 	default:
 		break;
--- include/game_of_life.h
+++ include/game_of_life.h
@@ -1,11 +1,5 @@
-#ifndef BADGE_APP_TEMPLATE_H__
-#define BADGE_APP_TEMPLATE_H__
-
-/*
-   To use this file, copy it to e.g. myapp.h, and in this new copy change BADGE_APP_TEMPLATE_H__
-   to MYAPP_H__ (or whatever you want to call it) and myapp_cb to your own chosen name,
-   and delete this comment from your copy.
-*/
+#ifndef GAME_OF_LIFE_H__
+#define GAME_OF_LIFE_H__
 
 void game_of_life_cb(void);
 
