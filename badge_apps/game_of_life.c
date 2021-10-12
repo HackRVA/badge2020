056724dcb472dc8518c3125c7efca1c5dbccdb9d
pacna <31336845+pacna@users.noreply.github.com>
2021-10-11 23:43:47 -0400

    Push what I have for now

--- badge_apps/game_of_life.c
+++ badge_apps/game_of_life.c
@@ -28,9 +28,20 @@ extern char *strcat(char *dest, const char *src);
 #include "build_bug_on.h"
 #include "xorshift.h"
 
+static int screen_changed = 0;
+
 #define GRID_SIZE 100
 #define ROW_SIZE 10
 #define COL_SIZE 10
+
+#define GRID_PADDING 3
+#define CELL_SIZE 20
+#define CELL_PADDING 0
+
+#define STARTX(x) (GRID_PADDING + CELL_PADDING + ((x)*CELL_SIZE))
+#define ENDX(x) (GRID_PADDING + CELL_SIZE + ((x)*CELL_SIZE))
+#define STARTY(y) (GRID_PADDING + CELL_PADDING + ((y)*CELL_SIZE))
+#define ENDY(y) (GRID_PADDING + CELL_SIZE + ((y)*CELL_SIZE))
 
 static struct Grid
 {
@@ -49,6 +60,7 @@ static struct Grid
 enum game_of_life_state_t
 {
 	GAME_OF_LIFE_INIT,
+	GAME_OF_LIFE_SPLASH_SCREEN,
 	GAME_OF_LIFE_RUN,
 	GAME_OF_LIFE_EXIT,
 };
@@ -87,76 +99,132 @@ static void init_grid()
 	}
 }
 
+static void render_box(int grid_x, int grid_y, int color)
+{
+	FbColor(color);
+	FbHorizontalLine(STARTX(grid_x), STARTY(grid_y), ENDX(grid_x), STARTY(grid_y));
+	FbHorizontalLine(STARTX(grid_x), ENDY(grid_y), ENDX(grid_x), ENDY(grid_y));
+	FbVerticalLine(STARTX(grid_x), STARTY(grid_y), STARTX(grid_x), ENDY(grid_y));
+	FbVerticalLine(ENDX(grid_x), STARTY(grid_y), ENDX(grid_x), ENDY(grid_y));
+}
+
+static void render_cell(int grid_x, int grid_y)
+{
+	render_box(grid_x, grid_y, WHITE);
+}
+
+static void render_grid()
+{
+
+	for (int row = 0; row <= 6 - 1; row++)
+	{
+
+		for (int col = 0; col < 6 - 1; col++)
+		{
+			render_cell(row, col);
+		}
+	}
+}
+
 static void debug_print()
 {
 	for (int i = 0; i < GRID_SIZE; i++)
 	{
-		printf("coordinate %d %d", grid.cells[i].coordinate.x, grid.cells[i].coordinate.y);
+		printf("%d %d %d \n", grid.cells[i].coordinate.x, grid.cells[i].coordinate.y, grid.cells[i].alive);
 	}
+}
+
+static void render_game()
+{
+	// if (!screen_changed)
+	// {
+	// 	return;
+	// }
+
+	// FbClear();
+
+	render_grid();
+
+	screen_changed = 0;
+}
+
+static void check_buttons()
+{
+	// if (BUTTON_PRESSED_AND_CONSUME)
+	// {
+	// 	screen_changed = 1;
+	// }
+	// else if (LEFT_BTN_AND_CONSUME)
+	// {
+	// 	screen_changed = 1;
+	// }
+	// else if (RIGHT_BTN_AND_CONSUME)
+	// {
+	// 	screen_changed = 1;
+	// }
+	// else if (UP_BTN_AND_CONSUME)
+	// {
+	// 	screen_changed = 1;
+	// }
+	// else if (DOWN_BTN_AND_CONSUME)
+	// {
+	// 	screen_changed = 1;
+	// }
+	FbColor(WHITE);
+	FbClear();
+	render_game();
+}
+
+static void render_splash_screen()
+{
+	FbColor(WHITE);
+	FbMove(10, LCD_YSIZE / 2);
+	FbWriteLine("Game of Life");
+	FbSwapBuffers();
 }
 
 static void game_of_life_init(void)
 {
 #ifdef __linux__
-	printf("grid size %d\nrow size: %d\n\n", GRID_SIZE, ROW_SIZE);
-#endif
 	FbInit();
 	FbClear();
-	game_of_life_state = GAME_OF_LIFE_RUN;
+
+	game_of_life_state = GAME_OF_LIFE_SPLASH_SCREEN;
+	printf("grid size %d row size: %d\n", GRID_SIZE, ROW_SIZE);
+#endif
 }
 
-static void check_buttons()
+static void game_of_life_splash_screen()
 {
+	render_splash_screen();
+
 	if (BUTTON_PRESSED_AND_CONSUME)
 	{
 		init_grid();
-		/* Pressing the button exits the program. You probably want to change this. */
-		game_of_life_state = GAME_OF_LIFE_EXIT;
+		game_of_life_state = GAME_OF_LIFE_RUN;
 	}
-	else if (LEFT_BTN_AND_CONSUME)
-	{
-	}
-	else if (RIGHT_BTN_AND_CONSUME)
-	{
-	}
-	else if (UP_BTN_AND_CONSUME)
-	{
-	}
-	else if (DOWN_BTN_AND_CONSUME)
-	{
-	}
-}
-
-static void draw_screen()
-{
-	FbColor(WHITE);
-	FbMove(10, 40 / 2);
-	FbWriteLine("Game of Life");
-	FbSwapBuffers();
 }
 
 static void game_of_life_run()
 {
 	check_buttons();
-	draw_screen();
 }
 
 static void game_of_life_exit()
 {
 	game_of_life_state = GAME_OF_LIFE_INIT; /* So that when we start again, we do not immediately exit */
-	debug_print();
 	returnToMenus();
 }
 
-static
-
-	int
-	game_of_life_cb(void)
+static int game_of_life_cb(void)
 {
 	switch (game_of_life_state)
 	{
 	case GAME_OF_LIFE_INIT:
 		game_of_life_init();
+		break;
+	case GAME_OF_LIFE_SPLASH_SCREEN:
+		game_of_life_splash_screen();
 		break;
 	case GAME_OF_LIFE_RUN:
 		game_of_life_run();
